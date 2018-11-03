e2d3e3018fad108b051a737efbb01b151bfe93a3
Henry <henrymzhao@gmail.com>
2018-11-03 01:01:53 -0700

    changed cmetadata shape, wrote initial settings retrieve

---
+++ src/aura/BaseComponent/BaseComponentController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Henry on 11/3/2018.
+ */
+({
+    init: function (cmp, evt, hlp) {
+        console.log('BaseComponentController.js ::', 'init');
+        hlp.getSettings(cmp);
+    }
+})
---
+++ src/aura/BaseComponent/BaseComponentHelper.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Henry on 11/3/2018.
+ */
+({
+    getSettings: function (cmp) {
+        return new Promise($A.getCallback(function(resolve, reject) {
+            let action = cmp.get('c.getSettings');
+            action.setCallback(this, function (a) {
+                let res = a.getState();
+                if (res === "SUCCESS") {
+                    let ret = a.getReturnValue();
+                    console.log('BaseComponentHelper.js ::', ret);
+                    cmp.set('v.pocket4d', ret);
+                    resolve(res);
+                } else {
+                    console.log('There was an error in BaseComponentHelper.js');
+                    console.log('STATE: ' + res, a.getError()[0].message);
+                    reject(res)
+                }
+            });
+            $A.enqueueAction(action);
+        }));
+    }
+})
